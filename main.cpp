14f7e36d0097e2d5884480d7908acc54317f3190
437042721@qq.com <437042721@qq.com>
2019-10-10 23:38:57 +0800

    updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <remuxer.h>
+#include <encode_video.h>
 int main() {
-    main_remuxer("../resources/video.avi","../resources/video.mp4");
+//    main_remuxer("../resources/video.avi","../resources/video.mp4");
+    main_encode_video("../resources/480x720.yuv","../resources/video.h264");
     system("pause");
     return 0;
 }
